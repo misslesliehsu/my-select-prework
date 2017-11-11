9a964fc15148bf37e9889af2d865beb851f3bae5
Leslie Hsu <chuckleberries@hotmail.com>
2017-11-11 22:57:49 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,11 +2,10 @@ def my_select(collection)
    i = 0
    selected = []
    while i < collection.length do
-     result = yield(collection[i])
-        if result == true
-          selected << collection[i]
-        end
-        i += 1
+      if yield(collection[i]) == true
+        selected << collection[i]
+      end
+      i += 1
     end
    return selected
  end
